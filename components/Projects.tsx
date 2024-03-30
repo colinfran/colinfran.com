1c4ca386c2b132fb585baafc8a01f4ba073f381e
Colin Franceschini <colin.franceschini@gmail.com>
2024-03-29 18:55:04 -0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { StaticImageData } from "next/image"
 import { Card } from "./ui/card"
 import Link from "next/link"
+import { v4 } from "uuid"
 
 type LinksProps = {
   url: string
@@ -24,7 +25,7 @@ export type ProjectsProps = {
 export const Projects: React.FC<ProjectsProps> = async ({ projects }) => (
   <div className="flex flex-col space-y-4">
     {projects.map(({ id, title, description }: ProjectProps) => (
-      <Link href={`/${id}`} key={id}>
+      <Link href={`/${id}`} key={v4()}>
         <Card className="flex cursor-pointer flex-col gap-2 p-8">
           <h3 className="font-semibold underline underline-offset-4">{title}</h3>
           <p className="text-muted-foreground">{description}</p>
