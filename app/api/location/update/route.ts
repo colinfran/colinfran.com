498d6093c2f3a12621a82f70da24a356ff22d589
Colin Franceschini <hello@colinfran.com>
2024-12-30 18:27:31 -0600

    Update route.ts

--- app/api/location/update/route.ts
+++ app/api/location/update/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server"
+import { NextResponse, NextRequest } from "next/server"
 
 /**
  * Handles GET requests to the '/api/location/update' endpoints.
@@ -6,23 +6,19 @@ import { NextResponse } from "next/server"
  * @returns {Promise<Response>} - Returns a response object.
  */
 
-export async function GET({
-  params,
-}: {
-  params: {
-    id?: string
-    longitude?: string
-    latitude?: string
-    city?: string
-    state?: string
-    country?: string
+export async function GET(request: NextRequest): Promise<Response> {
+  const searchParams = request.nextUrl.searchParams
+  const token = process.env.LOCATION_TOKEN!  
+  const id = searchParams.get('id')
+  const params = {
+    latitude: searchParams.get('latitude'),
+    longitude: searchParams.get('longitude'),
+    city: searchParams.get('city'),
+    state: searchParams.get('state'),
+    country: searchParams.get('country'),
   }
-}): Promise<Response> {
-  const token = process.env.LOCATION_TOKEN!
-  const id = params?.id || ""
-
   if (id !== token) {
-    return NextResponse.json({ error: "Invalid id", ...params, token }, { status: 400 })
+    return NextResponse.json({ error: "Invalid id" }, { status: 400 })
   }
 
   console.log(params)
