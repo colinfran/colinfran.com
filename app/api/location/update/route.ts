bf7f3890a0c78d991716228d0fd85cd1b1124124
Colin Franceschini <hello@colinfran.com>
2024-12-30 18:27:35 -0600

    Update route.ts

--- app/api/location/update/route.ts
+++ app/api/location/update/route.ts
@@ -8,14 +8,14 @@ import { NextResponse, NextRequest } from "next/server"
 
 export async function GET(request: NextRequest): Promise<Response> {
   const searchParams = request.nextUrl.searchParams
-  const token = process.env.LOCATION_TOKEN!  
-  const id = searchParams.get('id')
+  const token = process.env.LOCATION_TOKEN!
+  const id = searchParams.get("id")
   const params = {
-    latitude: searchParams.get('latitude'),
-    longitude: searchParams.get('longitude'),
-    city: searchParams.get('city'),
-    state: searchParams.get('state'),
-    country: searchParams.get('country'),
+    latitude: searchParams.get("latitude"),
+    longitude: searchParams.get("longitude"),
+    city: searchParams.get("city"),
+    state: searchParams.get("state"),
+    country: searchParams.get("country"),
   }
   if (id !== token) {
     return NextResponse.json({ error: "Invalid id" }, { status: 400 })
