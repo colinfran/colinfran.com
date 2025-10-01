c7194d93df265e2c4f2aa7230ac59100cd9601a9
Colin Franceschini <hello@colinfran.com>
2025-10-01 08:18:22 -0700

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -28,13 +28,13 @@ export const Navigation: FC = () => {
   return (
     <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4">
-        <div className="flex h-16 items-center justify-between">
+        <div className="m-4 flex flex-col md:flex-row h-16 items-center justify-between">
           <div className="flex items-center space-x-2">
             <Map className="h-6 w-6 text-primary" />
             <h1 className="text font-semibold">MyLocationAnalysis</h1>
           </div>
 
-          <div className="flex items-center space-x-1">
+          <div className="flex items-center space-x-1 pt-4">
             {navigationItems.map((item) => {
               const Icon = item.icon
               const isActive = pathname === item.href
