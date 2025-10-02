6703642b0e8549a5341c6251bb68d808107833a5
Colin Franceschini <hello@colinfran.com>
2025-10-01 17:47:57 -0700

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -28,13 +28,13 @@ export const Navigation: FC = () => {
   return (
     <nav className="border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto px-4">
-        <div className="m-4 flex flex-col md:flex-row h-16 items-center justify-between">
+        <div className="m-4 flex flex-col md:flex-row items-center justify-between gap-4">
           <div className="flex items-center space-x-2">
             <Map className="h-6 w-6 text-primary" />
             <h1 className="text font-semibold">MyLocationAnalysis</h1>
           </div>
 
-          <Tabs className="flex items-center space-x-1 pt-4" value={pathname}>
+          <Tabs className="flex items-center space-x-1" value={pathname}>
             <TabsList>
               {navigationItems.map((item) => {
                 const Icon = item.icon
